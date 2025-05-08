a5e762b0ca950db02510bfaede59870b0a836754
Kuldeep Soni <142746668+rjsoni107@users.noreply.github.com>
2025-05-08 18:07:29 +0530

    Code commit with fixes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const commonController = require('../controllers/commonController')
+<<<<<<< Updated upstream
 const mainController = require("../controllers/mainControllerMongo");
+=======
+const mainController = require("../controllers/mainController");
+>>>>>>> Stashed changes
 const { validateUser } = require("../middlewares/validation");
 const { verifyToken } = require("../middlewares/authMiddleware");
 const router = express.Router();
