7f71ea94d086734f664f62566b6da7c0a05d63c4
Kuldeep Soni <142746668+rjsoni107@users.noreply.github.com>
2025-05-09 14:09:18 +0530

    Fix Firestore security rules and test locally

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,21 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const userRoutes = require("./routes/userRoutes");
+const express = require('express');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
 const { createDefaultAdmin } = require("./controllers/mainController");
-
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-// Middleware
-app.use((req, res, next) => {
-    console.log(req.method, req.url);
-    next();
-});
+app.use('/crm', userRoutes);
 
-// Routes
-app.use("/crm", userRoutes);
-
-const PORT = process.env.PORT || 3005; // Default to 3005 if PORT is not set
+const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
+    createDefaultAdmin();
 });
 
-// MongoDB Connection
-const MONGO_SERVER = process.env.MONGO_SERVER;
-mongoose.connect(MONGO_SERVER)
-    .then(() => {
-        console.log("MongoDB Connected");
-        createDefaultAdmin();
-    })
-    .catch(err => console.log("MongoDB Connection Error:", err));
-
-// Export app for Vercel
 module.exports = app;
