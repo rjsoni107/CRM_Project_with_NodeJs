f7b35fc7a6c552ca6cb14adfcd82e7aabd4a9186
Kuldeep Soni <142746668+rjsoni107@users.noreply.github.com>
2025-05-15 18:10:22 +0530

    Added firebase admin sdk

--- backend/firebase.js
+++ backend/firebase.js
@@ -19,12 +19,25 @@
 
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
+require('dotenv').config();
 
 // Initialize Admin SDK with service account credentials
-const serviceAccount = require('./service-account-key.json');
+// const serviceAccount = require('./service-account-key.json');
 
 initializeApp({
-  credential: cert(serviceAccount),
+  // credential: cert(serviceAccount),
+  credential: cert({
+    projectId: process.env.GOOGLE_PROJECT_ID,
+    privateKey: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+    privateKeyId: process.env.GOOGLE_PRIVATE_KEY_ID,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    authUri: process.env.GOOGLE_AUTH_URI,
+    tokenUri: process.env.GOOGLE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    clientC509CertUrl: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    universeDomain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+  }),
 });
 
 const db = getFirestore();
