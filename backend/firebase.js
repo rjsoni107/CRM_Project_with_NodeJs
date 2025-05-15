37a4be9dfa1460cf4ec7b0f8c431d4a9b5e66078
Kuldeep Soni <142746668+rjsoni107@users.noreply.github.com>
2025-05-15 17:57:24 +0530

    Mergeing issue resolved

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,31 +1,8 @@
-// const { initializeApp } = require('firebase/app');
-// const { getFirestore } = require('firebase/firestore');
-// const dotenv = require("dotenv");
-// dotenv.config();
-
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-
-// module.exports = db;
-
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 require('dotenv').config();
 
-// Initialize Admin SDK with service account credentials
-// const serviceAccount = require('./service-account-key.json');
-
 initializeApp({
-  // credential: cert(serviceAccount),
   credential: cert({
     projectId: process.env.GOOGLE_PROJECT_ID,
     privateKey: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
